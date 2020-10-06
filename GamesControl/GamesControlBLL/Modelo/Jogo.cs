27ce5b2bc7fc5075bc2911039c30d880e9e6fafc
Paola Garbato <paolagarbato@gmail.com>
2020-10-06 16:57:23 -0300

    Criacao de classes da camada BLL

---
+++ GamesControl/GamesControlBLL/Modelo/Jogo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesControlBLL.Modelo
+{
+    public class Jogo : IntId
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public double Valor { get; set; }
+        public string  Imagem { get; set; }
+        public DateTime Data { get; set; }
+
+    }
+}
