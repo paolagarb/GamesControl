455eafaa1b59947c669cbe03ac4bef11338b8456
Paola Garbato <paolagarbato@gmail.com>
2020-10-06 21:12:14 -0300

    Implementando consulta de usuário

--- GamesControl/GamesControlDAL/Conexao.cs
+++ GamesControl/GamesControlDAL/Conexao.cs
@@ -5,21 +5,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data;
 
 namespace GamesControlDAL
 {
     public class Conexao
     {
-        public void Conectar()
+        public static string stringConexao = ConfigurationManager.ConnectionStrings["ConexaoSqlServer"].ConnectionString;
+        public static SqlConnection conexao = new SqlConnection(stringConexao);
+
+        public static void Conectar()
         {
-            string stringConexao = ConfigurationManager.ConnectionStrings["ConexaoSqlServer"].ConnectionString;
-            SqlConnection conn = new SqlConnection();
-            
+            try
+            {
+                if (conexao.State != ConnectionState.Open) conexao.Open();
+            } catch (SqlException e)
+            {
+                Console.WriteLine($"Erro de conexão: {e.Message}");
+            }
+           
         }
 
-        public void Desconectar()
+        public static void Desconectar()
         {
-
+            try
+            {
+                if (conexao.State == ConnectionState.Open) conexao.Close();
+            } catch (SqlException e)
+            {
+                Console.WriteLine($"Erro de conexão: {e.Message}");
+            }
         }
     }
 }
