8b885c3eb335854357d27793a4fe25901845b859
Paola Garbato <paolagarbato@gmail.com>
2020-10-06 22:36:34 -0300

    Adicionando referência > site > Entities/BLL pelo .csproject

--- GamesControl/GamesControl/Autenticacao/Login.aspx.cs
+++ GamesControl/GamesControl/Autenticacao/Login.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using GamesControlBLL.Autenticacao;
 
 namespace GamesControl.Autenticacao
 {
     public partial class Login : System.Web.UI.Page
     {
+        private LoginBo _loginBo;
         protected void Page_Load(object sender, EventArgs e)
         {
 
---
+++ GamesControl/GamesControlBLL/Autenticacao/LoginBo.cs
@@ -0,0 +1,27 @@
+﻿using GamesControlBLL.Exceptions;
+using GamesControlDAL;
+using Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesControlBLL.Autenticacao
+{
+    public class LoginBo
+    {
+        private UsuarioDAO _usuarioDao;
+        public Usuario ObterUsuario(string nomeUsuario, string senha)
+        {
+            Usuario usuario = _usuarioDao.ObterUsuario(nomeUsuario, senha);
+            if (usuario == null)
+            {
+                throw new UsuarioNaoCadastradoException();
+            } else
+            {
+                return usuario;
+            }
+        }
+    }
+}
